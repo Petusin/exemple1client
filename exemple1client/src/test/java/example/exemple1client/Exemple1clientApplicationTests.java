03e07098bad53ef6581ae12d86bd867d868616ec
Petusin <petusin1970@gmail.com>
2021-05-03 18:43:34 +0200

    commint 8082

---
+++ exemple1client/src/test/java/example/exemple1client/Exemple1clientApplicationTests.java
@@ -0,0 +1,13 @@
+package example.exemple1client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exemple1clientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
